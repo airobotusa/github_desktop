1f789dcbe2ddcd570a7eb46faf826bc30478076f
AIRobotUSA <35674826+airobotusa@users.noreply.github.com>
2018-02-23 12:37:08 -0500

    Add files via upload

---
+++ hello_world/swap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 1017;
+    int y = 50;
+    
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+    
+    // swapping...
+    int temp = x;
+    x = y;
+    y = temp;
+	// swapping is done
+	// merge conflicts
+	// merge made conflicts on purpose
+ 	// merge 2 for conflicts 222 333 444
+
+    printf("x is now %i\n", x);
+    printf("y is now %i\n", y);
+}
